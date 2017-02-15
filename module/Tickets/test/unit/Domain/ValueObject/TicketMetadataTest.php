c995f3cd14a12f7a6e0f84b47a30ec0bdbefe1f1
Carnage <t.carnage@gmail.com>
2017-02-15 18:41:22 +0000

    Added ticket metadata class

---
+++ module/Tickets/test/unit/Domain/ValueObject/TicketMetadataTest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace OpenTickets\Tickets\Domain\ValueObject;
+
+use OpenTickets\Tickets\Domain\Service\Configuration;
+use OpenTickets\Tickets\Domain\ValueObject\DiscountType\Percentage;
+
+class TicketMetadataTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @dataProvider provideAvailableOn
+     * @param $test
+     * @param $expected
+     */
+    public function testAvailableOn($test, $expected)
+    {
+        $ticketType = new TicketType('z', Price::fromNetCost(new Money(0, 'gbp'), new TaxRate(20)), 'Z');
+
+        $sut = TicketMetadata::createWithoutDates($ticketType);
+        $this->assertEquals($expected, $sut->isAvailableOn($test));
+    }
+
+    public function provideAvailableOn()
+    {
+        return [
+            [new \DateTime(), true],
+            [(new \DateTime())->sub(new \DateInterval('P2D')), false],
+            [(new \DateTime())->add(new \DateInterval('P2D')), false],
+        ];
+    }
+}
