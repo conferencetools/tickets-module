0e32b0deea822ee9a624d97e615b408f39c6ea31
Carnage <t.carnage@gmail.com>
2017-03-25 17:36:26 +0000

    Removed dead code

--- module/Tickets/src/Cli/Command/CancelTicket.php
+++ module/Tickets/src/Cli/Command/CancelTicket.php
@@ -3,18 +3,10 @@
 namespace OpenTickets\Tickets\Cli\Command;
 
 use Carnage\Cqrs\MessageBus\MessageBusInterface;
-use Carnage\Cqrs\Service\EventCatcher;
 use OpenTickets\Tickets\Domain\Command\Ticket\CancelTicket as CancelTicketCommand;
-use OpenTickets\Tickets\Domain\Command\Ticket\CompletePurchase;
-use OpenTickets\Tickets\Domain\Command\Ticket\ReserveTickets;
-use OpenTickets\Tickets\Domain\Event\Ticket\TicketPurchaseCreated;
-use OpenTickets\Tickets\Domain\Service\Configuration;
-use OpenTickets\Tickets\Domain\ValueObject\Delegate;
-use OpenTickets\Tickets\Domain\ValueObject\TicketReservationRequest;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
 class CancelTicket extends Command
@@ -23,16 +15,6 @@ class CancelTicket extends Command
      * @var MessageBusInterface
      */
     private $commandBus;
-
-    /**
-     * @var Configuration
-     */
-    private $config;
-
-    /**
-     * @var EventCatcher
-     */
-    private $eventCatcher;
 
     public static function build(MessageBusInterface $commandBus)
     {
