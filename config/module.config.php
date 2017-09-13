7d84c405fec5d7e2d964847a182c8bdca826a5a4
Carnage <t.carnage@gmail.com>
2017-09-13 17:40:21 +0100

    Rename layout to prevent conflict with other modules

--- config/module.config.php
+++ config/module.config.php
@@ -125,7 +125,7 @@ return [
         'not_found_template' => 'error/404',
         'exception_template' => 'error/index',
         'template_map' => [
-            'layout/layout' => __DIR__ . '/../view/layout/layout.phtml',
+            'tickets/layout' => __DIR__ . '/../view/layout/layout.phtml',
             'error/404' => __DIR__ . '/../view/error/404.phtml',
             'error/index' => __DIR__ . '/../view/error/index.phtml',
             'email/receipt' => __DIR__ . '/../view/email/receipt.phtml',
