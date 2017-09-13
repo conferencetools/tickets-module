a4dc86abb1935bcf419d7032469539e2e72e6b22
Carnage <t.carnage@gmail.com>
2017-09-13 18:02:22 +0100

    Tidied up composer files, added google analytics dependency to composer + module

--- src/Module.php
+++ src/Module.php
@@ -1,34 +1,18 @@
 <?php
-/**
- * Zend Framework (http://framework.zend.com/)
- *
- * @link      http://github.com/zendframework/ZendSkeletonApplication for the canonical source repository
- * @copyright Copyright (c) 2005-2014 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   http://framework.zend.com/license/new-bsd New BSD License
- */
 
 namespace ConferenceTools\Tickets;
 
-use Zend\Mvc\MvcEvent;
+use Zend\ModuleManager\Feature\DependencyIndicatorInterface;
 
-class Module
+class Module implements DependencyIndicatorInterface
 {
-    public function onBootstrap(MvcEvent $e)
+    public function getModuleDependencies()
     {
-        /*$eventManager        = $e->getApplication()->getEventManager();
-        $moduleRouteListener = new ModuleRouteListener();
-        $moduleRouteListener->attach($eventManager);
-
-        $t = $e->getTarget();
-
-        $t->getEventManager()->attach(
-            $t->getServiceManager()->get('ZfcRbac\View\Strategy\RedirectStrategy')
-        );*/
+        return ['ConferenceTools\GoogleAnalytics'];
     }
 
     public function getConfig()
     {
         return include __DIR__ . '/../config/module.config.php';
     }
-
 }
